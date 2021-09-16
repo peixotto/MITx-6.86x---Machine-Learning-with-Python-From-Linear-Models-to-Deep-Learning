e1883cd3da3f5ae31795ae44f0ff505261a02352
David Peixotto <peixotto@gmail.com>
2021-09-16 10:11:20 +0200

    First commit

---
+++ project0/norm.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def norm(A, B):
+    """
+    Takes two Numpy column arrays, A and B, and returns the L2 norm of their
+    sum.
+
+    Arg:
+      A - a Numpy array
+      B - a Numpy array
+    Returns:
+      s - the L2 norm of A+B.
+    """
+    #Your code here
+    #raise NotImplementedError
+
+    s = np.linalg.norm(A+B)
+
+    print('A:',A)
+    print('B:',B)
+    print('s:',s)
+    
+    return s
+
+
+X = np.random.random([2,2])
+Y = np.random.random([2,2])
+print(norm(X,Y))
+    
---
+++ project0/randomization.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def randomization(n):
+    """
+    Arg:
+      n - an integer
+    Returns:
+      A - a randomly-generated nx1 Numpy array.
+    """
+    #Your code here
+    A = np.random.random([n,1])
+    #raise NotImplementedError
+    return A
+
+print(randomization(5))
